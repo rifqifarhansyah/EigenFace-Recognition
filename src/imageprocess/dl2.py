42c61797d5a3c0f038e0015a65748350ff2508d6
Vieri Fajar Firdaus <16521294@mahasiswa.itb.ac.id>
2022-11-16 00:34:23 +0700

    fix imageprocess

--- src/imageprocess/dl2.py
+++ src/imageprocess/dl2.py
@@ -1,18 +1,19 @@
 import os
 from imageprocessing import *
-arr=os.listdir("test//Data_Set")
-print(arr)
-# image=cv2.imread("D:\Kuliah\Semester 3\IF2123 Aljabar Linier dan Geometri\Algeo02-21099\\test\Data_Set\pins_Anne Hathaway\Anne Hathaway82_482.jpg")
-cnt=1
-for i in arr :
-    # print("test//Data_Set"+str(i))
-    for path, currentDirectory, files in os.walk("test/Data_Set/"+str(i)):
-            for listfile in files:
-                print("test\Data_Set\\"+str(i)+"\\"+listfile)
-                image=cv2.imread("test\Data_Set\\"+str(i)+"\\"+listfile)
-                gray("test\Data_Set\\"+str(i)+"\\"+listfile,"test/Data_set_adjust/"+str(cnt)+".png")
-                cnt=cnt+1
-            # if(listfile[-3:]=="jpg" or listfile[-3:]=="png" or listfile[-4:]=="jpeg"):
-                # isExist = os.path.exists(pathname+'/'+listfile)
-                # croppicture(pathname+'/'+listfile,"result_picture/"+str(cnt)+".png")
-                # cnt+=1
+InputFolderWithCrop("test/Data_set/pins_amber heard")
+# arr=os.listdir("test//Data_Set")
+# print(arr)
+# # image=cv2.imread("D:\Kuliah\Semester 3\IF2123 Aljabar Linier dan Geometri\Algeo02-21099\\test\Data_Set\pins_Anne Hathaway\Anne Hathaway82_482.jpg")
+# cnt=1
+# for i in arr :
+#     # print("test//Data_Set"+str(i))
+#     for path, currentDirectory, files in os.walk("test/Data_Set/"+str(i)):
+#             for listfile in files:
+#                 print("test\Data_Set\\"+str(i)+"\\"+listfile)
+#                 image=cv2.imread("test\Data_Set\\"+str(i)+"\\"+listfile)
+#                 gray("test\Data_Set\\"+str(i)+"\\"+listfile,"test/Data_set_adjust/"+str(cnt)+".png")
+#                 cnt=cnt+1
+#             # if(listfile[-3:]=="jpg" or listfile[-3:]=="png" or listfile[-4:]=="jpeg"):
+#                 # isExist = os.path.exists(pathname+'/'+listfile)
+#                 # croppicture(pathname+'/'+listfile,"result_picture/"+str(cnt)+".png")
+#                 # cnt+=1
